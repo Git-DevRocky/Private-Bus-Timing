b62ee9e00ab0a0e437b35d70ed2b3b76141001ca
Git-DevRocky <vyasanbmathew5@gmail.com>
2025-09-29 21:33:07 +0530

    fix: corrected Foooter.js

--- src/components/Foooter.js
+++ src/components/Foooter.js
@@ -3,9 +3,9 @@ import React from "react";
 function Foooter() {
   return (
     <div className="fixed  bottom-0 bg-black bg-opacity-35 h-5 w-full text-white  flex  items-center justify-center">
-      <p className="text-xs mb-1">All Rights Reserved &#169; Albin Sabu 2024</p>{" "}
+      <p className="text-xs mb-1">Follow Me On Insta</p>{" "}
       <a
-        href="https://github.com/albinsabu2023/Kerala-Private-Bus-Timing-App/"
+        href="https://Instagram.com/vyasanbmathew__"
         className="text-xs  text-blue-800 px-3 mb-1"
       >
         github
